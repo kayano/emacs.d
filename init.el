23fa345d99fceb7add974519bf4c081d0d9b3836
Tomasz Nowak <tomasz.nowak@getbase.com>
2018-04-26 22:59:06 +0200

    Change init.el for golang

--- init.el
+++ init.el
@@ -1,31 +1,114 @@
-(setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-	("marmalade" . "http://marmalade-repo.org/packages/")
-	("melpa" . "http://melpa.milkbox.net/packages/")))
+;; Who am I?
 
-(defconst themes-dir (expand-file-name "themes/" user-emacs-directory)
-  "The root folder of themes")
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
-(defun add-subfolders-to-load-path (parent-dir)
-  "Add subfolders to load path"
-  (dolist (f (directory-files parent-dir))
-    (let ((name (concat parent-dir f)))
-      (when (and (file-directory-p name)
-                 (not (equal f ".."))
-                 (not (equal f ".")))
-        (add-to-list 'load-path name)))))
+(setq user-full-name "Tomasz Nowak")
+(setq user-mail-address "tomasz.nowak@getbase.com")
+(setq global-mode-string (message "  %s  " user-full-name))
 
-(defun add-subfolders-to-theme-load-path (parent-dir)
-  "Add subfolders to theme load path"
-  (dolist (f (directory-files parent-dir))
-    (let ((name (concat parent-dir f)))
-      (when (and (file-directory-p name)
-                 (not (equal f ".."))
-                 (not (equal f ".")))
-        (add-to-list 'custom-theme-load-path name)))))
+;; UI
+(tool-bar-mode -1)                  ; Disable the button bar atop screen
+(scroll-bar-mode -1)                ; Disable scroll bar
+(setq inhibit-startup-screen t)     ; Disable startup screen with graphics
+(set-default-font "Monaco:pixelsize=14")      ; Set font and size
+(setq-default indent-tabs-mode nil) ; Use spaces instead of tabs
+(setq tab-width 2)                  ; Four spaces is a tab
+(setq visible-bell nil)             ; Disable annoying visual bell graphic
+(setq ring-bell-function 'ignore)   ; Disable super annoying audio bell
 
-;; Add the first level subfolders automatically
-(add-subfolders-to-load-path user-emacs-directory)
+;; Set frame title
+(setq frame-title-format '(buffer-file-name "Emacs: %b (%f)" "Emacs: %b"))
 
-;; Load configuration
-(require 'tn-ui)                ; eye candy
+;; Make keyboard bindings not suck
+;;(setq mac-option-modifier 'super)
+;;(setq mac-command-modifier 'meta)
+(global-set-key "\M-c" 'copy-region-as-kill)
+(global-set-key "\M-v" 'yank)
+(global-set-key "\M-g" 'goto-line)
+(global-set-key (kbd "M-0") 'delete-window)
+(global-set-key (kbd "M-1") 'delete-other-windows)
+(global-set-key (kbd "M-2") 'split-window-vertically)
+(global-set-key (kbd "M-3") 'split-window-horizontally)
+(global-set-key (kbd "M-4") 'delete-other-windows-vertically)
+(global-set-key (kbd "M-o") 'other-window)
+(global-set-key (kbd "M-O") (lambda () (interactive) (other-window -1))) ; back one
+(global-set-key (kbd "M-k") 'kill-this-buffer)
+;;; Font size
+(global-set-key (kbd "C-x C-+") 'text-scale-increase)
+(global-set-key (kbd "C-x C--") 'text-scale-decrease)
+
+;; Set up package repositories so M-x package-install works.
+(require 'package) 
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(load-theme 'solarized-dark t)       ; Color theme installed via melpa
+
+;; Add a directory to the load path so we can put extra files there
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;; Snag the user's PATH and GOPATH
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-env "GOPATH"))
+
+;; Define function to call when go-mode loads
+(defun my-go-mode-hook ()
+  (add-hook 'before-save-hook 'gofmt-before-save) ; gofmt before every save
+  (setq gofmt-command "goimports")                ; gofmt uses invokes goimports
+  (if (not (string-match "go" compile-command))   ; set compile command default
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+
+  ;; guru settings
+  (go-guru-hl-identifier-mode)                    ; highlight identifiers
+  
+  ;; Key bindings specific to go-mode
+  (local-set-key (kbd "M-.") 'godef-jump)         ; Go to definition
+  (local-set-key (kbd "M-*") 'pop-tag-mark)       ; Return from whence you came
+  (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
+  (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
+  (local-set-key (kbd "M-]") 'next-error)         ; Go to next error (or msg)
+  (local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
+
+  ;; Misc go stuff
+  (auto-complete-mode 1))                         ; Enable auto-complete mode
+
+;; Connect go-mode-hook with the function we just defined
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+;; Ensure the go specific autocomplete is active in go-mode.
+(with-eval-after-load 'go-mode
+   (require 'go-autocomplete))
+
+;; If the go-guru.el file is in the load path, this will load it.
+(require 'go-guru)
+
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
+
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+;; Emacs backups
+(setq backup-directory-alist `(("." . "~/.saves")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode groovy-mode toml-mode window-number solarized-theme smooth-scroll project-explorer neotree markdown-mode json-mode golint go-eldoc go-autocomplete flymake-go exec-path-from-shell buffer-move))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
